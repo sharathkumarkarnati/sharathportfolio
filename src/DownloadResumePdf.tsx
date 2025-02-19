a2121bd0c19685542425f6da9c73d4be23d03a1d
Sharathkarnati2100 <sharathkarnati21@gmail.com>
2025-02-18 19:17:13 -0800

    f

--- src/DownloadResumePdf.tsx
+++ src/DownloadResumePdf.tsx
@@ -1,9 +1,10 @@
 import { HiOutlineDocumentArrowDown } from "react-icons/hi2";
+import resumepdf from "./Sharath_Karnati_Resume.pdf?url";
 
 export const DownloadResumePdf = () => {
   return (
     <a
-      href="/Sharath_Karnati_Resume.pdf"
+      href={resumepdf}
       download="Sharath_Karnati_Resume.pdf"
       className={`flex flex-row justify-center items-center text-base md:text-lg font-semibold cursor-pointer
                     p-2 pl-0 rounded-lg hover:underline`}
